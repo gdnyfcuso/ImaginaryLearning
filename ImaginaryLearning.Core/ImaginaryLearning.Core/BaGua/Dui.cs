27d94030be7aecfdcc2736f3ff1a73ddccfb5560
LXZ <gdnyfcuso@163.com>
2019-07-02 21:47:31 +0800

    画太极图

---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/BaGua/Dui.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core.BaGua
+{
+    /// <summary>
+    /// 泽
+    /// </summary>
+    public class Dui : DanGua
+    {
+        public Dui(Point startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            ChuYao = new YangYao(CreateThirdPoint(startPoint, heigth), totalWidth, heigth);
+            ErYao = new YangYao(CreateSecondPoint(startPoint, heigth), totalWidth, heigth);
+            SanYao = new YinYao(startPoint, totalWidth, midWidth, heigth);
+            GuaRectangle = GetGuaRectangle(totalWidth, heigth, midWidth);
+            Name = "泽";
+        }
+
+        public Dui(Point startPoint) : this(startPoint, GuaConst.TotalWidth, GuaConst.MidWidth, GuaConst.Heigth)
+        {
+
+        }
+
+        public Dui() : this(new Point() { X = GuaConst.Start_Point_X, Y = GuaConst.Start_Point_Y })
+        {
+
+        }
+    }
+}
