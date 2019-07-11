cabd68f7aaba94a4942ab30cec2cb3231e799f09
LXZ <gdnyfcuso@163.com>
2019-07-11 22:01:25 +0800

    添加坐标系统

--- ImaginaryLearning.Core/ImaginaryLearning.Core/Base/BaguaCoordinateSystem.cs
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/Base/BaguaCoordinateSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace ImaginaryLearning.Core.Base
@@ -21,5 +22,53 @@ namespace ImaginaryLearning.Core.Base
 
         //var tmpX = x + r * Math.Cos(angle * 3.14 / 180);
         //var tmpY = y + r * Math.Sin(angle * 3.14 / 180);
+
+        public BaguaCoordinateSystem(Point point, int r = 300)
+        {
+
+        }
+
+        /// <summary>
+        /// 卦的起始位置 正东
+        /// </summary>
+        public Point ZD { get; set; }
+
+        /// <summary>
+        /// 正西
+        /// </summary>
+        public Point ZX { get; set; }
+
+
+        /// <summary>
+        /// 卦的起始位置 正南
+        /// </summary>
+        public Point ZN { get; set; }
+
+        /// <summary>
+        /// 正北
+        /// </summary>
+        public Point ZB { get; set; }
+
+        /// <summary>
+        /// 卦的起始位置 东南
+        /// </summary>
+        public Point DN { get; set; }
+
+        /// <summary>
+        /// 西南
+        /// </summary>
+        public Point XN { get; set; }
+
+
+        /// <summary>
+        /// 卦的起始位置 东北
+        /// </summary>
+        public Point DB { get; set; }
+
+        /// <summary>
+        /// 西北
+        /// </summary>
+        public Point XB { get; set; }
+
     }
 }
