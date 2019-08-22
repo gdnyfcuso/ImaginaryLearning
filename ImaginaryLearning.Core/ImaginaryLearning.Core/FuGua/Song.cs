9a4dce58460a635c38722ac29fad77b577943dbf
LXZ <gdnyfcuso@163.com>
2019-08-22 21:02:16 +0800

    添加讼卦和需卦

---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/FuGua/Song.cs
@@ -0,0 +1,54 @@
+﻿using ImaginaryLearning.Core.BaGua;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core
+{
+    /// <summary>
+    /// 天水讼 讼卦，象征争论、诉讼。
+    /// 乾为天，坎为水，天西转与水东流背向而行，像人与人不和而争辩。
+    /// 讼象征争辩争论，含诉讼之义。
+    /// 当不易和解时，便会导致诉讼。
+    /// 应该找有大德大才的人进行决断，不要逞强冒险。
+    /// </summary>
+    public class Song : FuGua
+    {
+        public Song(PointF startPoint, int totalWidth, int midWidth, int heigth) : base(startPoint, totalWidth, midWidth, heigth)
+        {
+        }
+
+        protected override void InitFuGua(PointF startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            XiaGua = new Kan(CreateXiaGuaPoint(startPoint, heigth), totalWidth, midWidth, heigth);
+            ShangGua = new Qian(startPoint, totalWidth, heigth);
+            Name = "讼";
+            PinYin = "Song";
+            XuGua = 6;
+            GuaCi = "有孚窒惕，中吉，终凶。利见大人。不利涉大川。";
+            TuanCi = "《彖》曰：讼，上刚下险，险而健，讼。“讼有孚窒惕，中吉”，刚来而得中也。“终凶”，讼不可成也。“利见大人”，尚中正也。“不利涉大川”，入于渊也。";
+            GuaXiangCi = "《象》曰：天与水违行，讼。君子以作事谋始。";
+            YaoCi = new string[]
+            {
+                "初六，不永所事，小有言，终吉。",
+                "九二，不克讼，归而逋。其邑人三百户，无眚。",
+                "六三，食旧德，贞厉，终吉。或従王事，无成。六三，食旧德，贞厉，终吉。或従王事，无成。",
+                "九四，不克讼，复既命渝。安贞吉。",
+                "九五：讼，元吉。",
+                "上九：或锡之鞶带，终朝三褫之。"
+
+            };
+            XiangCi = new string[]
+            {
+                "《象》曰：“不永所事”，讼不可长也。虽“小有言”，其辩明也。",
+                "《象》曰：“不克讼”，归逋窜也。自下讼上，患至掇也。",
+                "《象》曰：食旧德，従上吉也。",
+                "《象》曰：复即命渝，安贞不失也。",
+                "《象》曰：“讼，元吉”以中正也。",
+                "《象》曰：以讼受服，亦不足敬也。"
+            };
+
+        }
+    }
+}
---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/FuGua/Xu.cs
@@ -0,0 +1,50 @@
+﻿using ImaginaryLearning.Core.BaGua;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core
+{
+    /// <summary>
+    ///水天需 坎上乾下
+    /// </summary>
+    public class Xu : FuGua
+    {
+        public Xu(PointF startPoint, int totalWidth, int midWidth, int heigth) : base(startPoint, totalWidth, midWidth, heigth)
+        {
+        }
+
+        protected override void InitFuGua(PointF startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            ShangGua = new Kan(startPoint, totalWidth, midWidth, heigth);
+            XiaGua = new Qian(CreateXiaGuaPoint(startPoint, heigth), totalWidth, heigth);
+            Name = "需";
+            PinYin = "Xu";
+            XuGua = 5;
+            GuaCi = "有孚，光亨。贞吉，利涉大川。";
+            TuanCi = "《彖》曰：“需”，须也。险在前也，刚健而不陷，其义不困穷矣。" +
+                "“需，有孚，光亨，贞吉”，位乎天位，以正中也。“利涉大川”，往有功也。";
+            GuaXiangCi = "《象》曰：云上于天，需。君子以饮食宴乐。";
+            YaoCi = new string[]
+            {
+                "初九，需于郊，利用恒，无咎。",
+                "九二，需于沙，小有言，终吉。",
+                "九三，需于泥，致寇至。",
+                "六四，需于血，出自穴。",
+                "九五，需于酒食，贞吉。",
+                "上六，入于穴，有不速之客三人来，敬之终吉。"
+            };
+
+            XiangCi = new string[]
+            {
+                " 《象》曰：“需于郊”，不犯难行也。“利用恒无咎”，未失常也。",
+                "《象》曰：“需于沙”，衍在中也。虽小有言，以终吉也。",
+                "《象》曰：“需于泥”，灾在外也。自我致寇，敬慎不败也。",
+                "《象》曰：“需于血，”顺以听也。",
+                "《象》曰：“酒食贞吉”，以中正也。",
+                "《象》曰：“不速之客来，敬之终吉”，虽不当位，未大失也。"
+            };
+        }
+    }
+}
