ce3974ba34b9f9ecd87249c4c7b596ac4b5f6fe2
LXZ <gdnyfcuso@163.com>
2019-09-27 21:07:10 +0800

    添加新的卦

---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/FuGua/DaXu.cs
@@ -0,0 +1,49 @@
+﻿using ImaginaryLearning.Core.BaGua;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core
+{
+    /// <summary>
+    /// 山天
+    /// </summary>
+    public class DaXu : FuGua
+    {
+        public DaXu(PointF startPoint, int totalWidth, int midWidth, int heigth) : base(startPoint, totalWidth, midWidth, heigth)
+        {
+        }
+
+        protected override void InitFuGua(PointF startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            ShangGua = new Gen(startPoint, totalWidth, midWidth, heigth);
+            XiaGua = new Qian(CreateXiaGuaPoint(startPoint, heigth), totalWidth, heigth);
+            Name = "大畜";
+            PinYin = "DaXu";
+            XuGua = 32;
+            GuaCi = "利贞。不家食吉。利涉大川。";
+            TuanCi = "《彖》曰：大畜，刚健笃实，辉光日新。其德刚上而尚贤，能止健，大正也。“不家食吉”，养贤也。“利涉大川”，应乎天也。";
+            GuaXiangCi = "《象》曰：天在山中，大畜。君子以多识前贤往行，以畜其德。";
+            YaoCi = new string[]
+            {
+                "初九，有厉，利已。",
+                "九二，舆说輹。",
+                "九三，良马逐，利艰贞，曰闲舆卫，利有攸往。",
+                "六四，童牛之牿，元吉。",
+                "六五，豮豕之牙，吉。",
+                "上九，何天之衢，亨。"
+            };
+
+            XiangCi = new string[]
+            {
+                "《象》曰：“有厉利已”，不犯灾也。",
+                "《象》曰：“舆说輹”，中无尤也。",
+                "《象》曰：“利有攸往”，上合志也。",
+                "《象》曰：“六四元吉”，有喜也。",
+                "《象》曰：“六五之吉”，有庆也。",
+                "《象》曰：“何天之衢”，道大行也。"
+            };
+        }
+    }
+}
---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/FuGua/Yi.cs
@@ -0,0 +1,47 @@
+﻿using ImaginaryLearning.Core.BaGua;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core
+{
+    public class Yi : FuGua
+    {
+        public Yi(PointF startPoint, int totalWidth, int midWidth, int heigth) : base(startPoint, totalWidth, midWidth, heigth)
+        {
+        }
+
+        protected override void InitFuGua(PointF startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            ShangGua = new Gen(startPoint, totalWidth, midWidth, heigth);
+            XiaGua = new Zhen(CreateXiaGuaPoint(startPoint, heigth), totalWidth, midWidth, heigth);
+            Name = "颐";
+            PinYin = "Yi";
+            XuGua = 33;
+            GuaCi = "贞吉。观颐，自求口实。";
+            TuanCi = "《彖》曰：颐，贞吉，养正则吉也。观颐，观其所养也。自求口实，观其自养也。天地养万物，圣人养贤以及万民，颐之时大矣哉！";
+            GuaXiangCi = "《象》曰：山下有雷，颐。君子以慎言语，节饮食。";
+            YaoCi = new string[]
+            {
+                "初九，舍尔灵龟，观我朵颐，凶。",
+                "六二，颠颐拂经于丘颐，征凶。",
+                "六三，拂颐，贞凶，十年勿用，无攸利。",
+                "六四，颠颐，吉。虎视眈眈，其欲逐逐，无咎。",
+                "六五，拂经，居贞吉，不可涉大川。",
+                "上九，由颐，厉，吉。利涉大川。"
+            };
+
+            XiangCi = new string[]
+            {
+                "《象》曰：“观我朵颐”，亦不足贵也。",
+                "《象》曰：“六二征凶”，行失类也。",
+                "《象》曰：“十年勿用”，道大悖也。",
+                "《象》曰：“颠颐之吉”，上施光也。",
+                "《象》曰：“居贞之吉”，顺以従上也。",
+                "《象》曰：“由颐厉吉”，大有庆也。"
+            };
+            
+        }
+    }
+}
