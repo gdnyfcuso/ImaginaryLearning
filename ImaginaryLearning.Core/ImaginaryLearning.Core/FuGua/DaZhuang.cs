5363d0fdf58268b6c90bf4842601f796ba1e4754
gdnyfcuso <gdnyfcuso@163.com>
2019-10-03 14:49:16 +0800

    添加遁卦

---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/FuGua/DaZhuang.cs
@@ -0,0 +1,45 @@
+﻿using ImaginaryLearning.Core.BaGua;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core
+{
+    public class DaZhuang : FuGua
+    {
+        public DaZhuang(PointF startPoint, int totalWidth, int midWidth, int heigth) : base(startPoint, totalWidth, midWidth, heigth)
+        {
+        }
+
+        protected override void InitFuGua(PointF startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            ShangGua = new Gen(startPoint, totalWidth, midWidth, heigth);
+            XiaGua = new Qian(CreateXiaGuaPoint(startPoint, heigth), totalWidth, heigth);
+            Name = "大壮";
+            PinYin = "DaZhuang";
+            XuGua = 36;
+            GuaCi = "利贞。";
+            TuanCi = "《彖》曰：大壮，大者壮也。刚以动，故壮。“大壮利贞”，大者正也。正大，而天地之情可见矣。";
+            GuaXiangCi = "《象》曰：雷在天上，大壮。君子以非礼弗履。";
+            YaoCi = new string[]
+            {
+                "初九，壮于趾，征凶，有孚。",
+                "九二，贞吉。",
+                "九三，小人用壮，君子用罔，贞厉。羝羊触藩，羸其角。",
+                "九四，贞吉，悔亡。藩决不羸，壮于大舆之輹。",
+                "六五，丧羊于易，无悔。",
+                "上六，羝羊触藩，不能退，不能遂，无攸利，艰则吉。"
+            };
+            XiangCi = new string[] {
+
+                "《象》曰：“壮于趾”，其孚穷也。",
+                "《象》曰：九二“贞吉”，以中也。",
+                "《象》曰：“小人用壮”，君子以罔也。",
+                "《象》曰：“藩决不羸”，尚往也。",
+                "《象》曰：“丧羊于易”，位不当也。",
+                "《象》曰：“不能退，不能遂”，不详也。“艰则吉”，咎不长也。"
+            };
+      }
+    }
+}
---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/FuGua/Dun.cs
@@ -0,0 +1,49 @@
+﻿using ImaginaryLearning.Core.BaGua;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core
+{
+    /// <summary>
+    /// 天山遁
+    /// </summary>
+    public class Dun : FuGua
+    {
+        public Dun(PointF startPoint, int totalWidth, int midWidth, int heigth) : base(startPoint, totalWidth, midWidth, heigth)
+        {
+        }
+
+        protected override void InitFuGua(PointF startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            ShangGua = new Qian(startPoint, totalWidth, heigth);
+            XiaGua = new Gen(CreateXiaGuaPoint(startPoint, heigth), totalWidth, midWidth, heigth);
+            Name = "遁";
+            PinYin = "Dun";
+            XuGua = 35;
+            GuaCi = "亨。小利贞。";
+            TuanCi = "《彖》曰：“遁亨”，遁而亨也。刚当位而应，与时行也。“小利贞”，浸而长也。遁之时义大矣哉！斋";
+            GuaXiangCi = "《象》曰：天下有山，遁。君子以远小人，不恶而严。";
+            YaoCi = new string[]
+            {
+                "初六，遁尾，厉，勿用有攸往。",
+                "六二，执之用黄牛之革，莫之胜说。",
+                "九三，系遁，有疾厉，畜臣妾吉。",
+                "九四，好遁，君子吉，小人否。",
+                "九五，嘉遁，贞吉。",
+                "上九，肥遁，无不利。"
+            };
+            XiangCi = new string[]
+            {
+                "《象》曰：“遁尾”之“厉”，不往何灾也？古",
+                "《象》曰：“执用黄牛”，固志也。",
+                "《象》曰：“系遁”之“厉”，有疾惫也。“畜臣妾吉”，不可大事也。",
+                "《象》曰：“君子好遁，小人否”也。",
+                "《象》曰：“嘉遁贞吉”，以正志也",
+                "《象》曰：“肥遁无不利”，无所疑也。"
+            };
+
+        }
+    }
+}
