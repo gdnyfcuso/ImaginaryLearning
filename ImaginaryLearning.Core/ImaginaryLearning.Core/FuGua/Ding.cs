bd43b21640315e5ecc480084f59a9d31b5785f79
LXZ <gdnyfcuso@163.com>
2019-10-15 20:55:52 +0800

    iglk

---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/FuGua/Ding.cs
@@ -0,0 +1,49 @@
+﻿using ImaginaryLearning.Core.BaGua;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core
+{
+    /// <summary>
+    /// 火风鼎
+    /// </summary>
+    public class Ding : FuGua
+    {
+        public Ding(PointF startPoint, int totalWidth, int midWidth, int heigth) : base(startPoint, totalWidth, midWidth, heigth)
+        {
+        }
+
+        protected override void InitFuGua(PointF startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            ShangGua = new Li(startPoint, totalWidth, midWidth, heigth);
+            XiaGua = new Xun(CreateXiaGuaPoint(startPoint, heigth), totalWidth, midWidth, heigth);
+            Name = "鼎";
+            PinYin = "Ding";
+            XuGua = 50;
+            GuaCi = "元吉，亨。";
+            TuanCi = "《彖》曰：鼎，象也。以木巽火，亨饪也。圣人亨以享上帝，而大亨以养圣贤。巽而耳目聪明，柔进而上行，得中而应乎刚，是以元亨。";
+            GuaXiangCi = "《象》曰：木上有火，鼎。君子以正位凝命。";
+            YaoCi = new string[]
+            {
+                "初六，鼎颠趾，利出否。得妾以其子，无咎。",
+                "九二，鼎有实，我仇有疾，不我能即，吉。",
+                "九三，鼎耳革，其行塞，雉膏不食，方雨，亏悔，终吉。",
+                "九四，鼎折足，覆公餗，其形渥，凶。",
+                "六五，鼎黄耳金铉，利贞。",
+                "上九，鼎玉铉，大吉，无不利。"
+
+            };
+            XiangCi = new string[]
+            {
+                "《象》曰：“鼎颠趾”，未悖也。“利出否”，以従贵也。",
+                "《象》曰：“鼎有实”，慎所之也。“我仇有疾”，终无尤也。",
+                "《象》曰：“鼎耳革”，失其义也。",
+                "《象》曰：“覆公餗”，信如何也。",
+                "《象》曰：“鼎黄耳”，中以为实也。",
+                "《象》曰：玉铉在上，刚柔节也。"
+            };
+        }
+    }
+}
---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/FuGua/Ge.cs
@@ -0,0 +1,45 @@
+﻿using ImaginaryLearning.Core.BaGua;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core
+{
+    public class Ge : FuGua
+    {
+        public Ge(PointF startPoint, int totalWidth, int midWidth, int heigth) : base(startPoint, totalWidth, midWidth, heigth)
+        {
+        }
+
+        protected override void InitFuGua(PointF startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            ShangGua = new Dui(startPoint,totalWidth,midWidth,heigth);
+            XiaGua = new Li(CreateXiaGuaPoint(startPoint, heigth), totalWidth, midWidth, heigth);
+            Name = "革";
+            PinYin = "Ge";
+            XuGua = 49;
+            GuaCi = "已日乃孚。元亨。利贞，悔亡。";
+            TuanCi = "《彖》曰：革，水火相息，二女同居，其志不相得曰革。“已日乃孚”，革而信之。文明以说，大亨以正。革而当，其悔乃亡。天地革而四时成，汤武革命，顺乎天而应乎人。革之时大矣哉！";
+            GuaXiangCi = "《象》曰：泽中有火，革。君子以治历明时。";
+            YaoCi = new string[]
+            {
+                "初九，巩用黄牛之革。",
+                "六二，巳日乃革之，征吉，无咎。",
+                "九三，征凶。贞厉。革言三就，有孚。",
+                "九四，悔亡。有孚改命，吉。",
+                "九五，大人虎变，未占有孚。",
+                "上六，君子豹变，小人革面，征凶，居贞吉。"
+            };
+            XiangCi = new string[]
+            {
+                "《象》曰：“巩用黄牛”，不可以有为也。",
+                "《象》曰：“巳日革之”，行有嘉也。",
+                "《象》曰：“革言三就”，又何之矣。",
+                "《象》曰：“改命之吉”，信志也。",
+                "《象》曰：“大人虎变”，其文炳也。",
+                "《象》曰：“君子豹变”，其文蔚也。“小人革面”，顺以从君也。"
+            };
+        }
+    }
+}
