4f9694a523303a4edc9f554bfc9fe9a948c2947b
LXZ <gdnyfcuso@163.com>
2019-10-22 21:39:09 +0800

    添加两点卦

---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/FuGua/GuiMei.cs
@@ -0,0 +1,49 @@
+﻿using ImaginaryLearning.Core.BaGua;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core
+{
+    /// <summary>
+    /// 雷泽归妹
+    /// </summary>
+    public class GuiMei : FuGua
+    {
+        public GuiMei(PointF startPoint, int totalWidth, int midWidth, int heigth) : base(startPoint, totalWidth, midWidth, heigth)
+        {
+        }
+
+        protected override void InitFuGua(PointF startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            ShangGua = new Zhen(startPoint, totalWidth, midWidth, heigth);
+            XiaGua = new Dui(CreateXiaGuaPoint(startPoint, heigth), totalWidth, midWidth, heigth);
+            Name = "归妹";
+            PinYin = "GuiMei";
+            XuGua = 54;
+            GuaCi = "征凶，无攸利。";
+            GuaXiangCi = "《象》曰：泽上有雷，归妹。君子以永终知敝。";
+            TuanCi = "《彖》曰：归妹，天地之大义也。天地不交而万物不兴。归妹，人之终始也。说以动，所归妹也。“征凶”，位不当也。“无攸利”，柔乘刚也。";
+            YaoCi = new string[]
+            {
+                "初九，归妹以娣。跛能履，征吉。",
+                "九二，眇能视，利幽人之贞。",
+                "六三，归妹以须，反归以娣。",
+                "九四，归妹愆期，迟归有时。",
+                "六五，帝乙归妹，其君之袂不如其娣之袂良。月几望，吉。",
+                "上六，女承筐无实，士刲羊无血，无攸利。"
+            };
+            XiangCi = new string[]
+            {
+                "《象》曰：“归妹以娣”，以恒也。“跛能履吉”，相承也。",
+                "《象》曰：“利幽人之贞”，未变常也。",
+                "《象》曰：“归妹以须”，未当也。",
+                "《象》曰：“愆期”之志，有待而行也。",
+                "《象》曰：“帝乙归妹，不如其娣之袂良”也。其位在中，以贵行也。",
+                "《象》曰：上六无实，承虚筐也。"
+            };
+
+        }
+    }
+}
---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/FuGua/JianA.cs
@@ -0,0 +1,48 @@
+﻿using ImaginaryLearning.Core.BaGua;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core
+{
+    /// <summary>
+    /// 风山渐
+    /// </summary>
+    class JianA : FuGua
+    {
+        public JianA(PointF startPoint, int totalWidth, int midWidth, int heigth) : base(startPoint, totalWidth, midWidth, heigth)
+        {
+        }
+
+        protected override void InitFuGua(PointF startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            ShangGua = new Xun(startPoint,totalWidth,midWidth,heigth);
+            XiaGua = new Gen(CreateXiaGuaPoint(startPoint, heigth), totalWidth, midWidth, heigth);
+            Name = "渐";
+            PinYin = "Jian";
+            GuaCi = "女归吉，利贞。";
+            XuGua = 53;
+            GuaXiangCi = "《象》曰：山上有木，渐。君子以居贤德善俗。";
+            TuanCi = "《彖》曰：渐之进也，女归吉也。进得位，往有功也。进以正，可以正邦也。其位刚得中也。止而巽，动不穷也。";
+            YaoCi = new string[]
+            {
+                "初六，鸿渐于干。小子厉，有言，无咎。",
+                "六二，鸿渐于磐，饮食衎衎，吉。",
+                "九三，鸿渐于陆。夫征不复，妇孕不育，凶。利御寇。",
+                "六四，鸿渐于木，或得其桷，无咎。",
+                "九五，鸿渐于陵，妇三岁不孕，终莫之胜，吉。",
+                "上九，鸿渐于陆，其羽可用为仪，吉。"
+            };
+            XiangCi = new string[]
+            {
+                "《象》曰：“小子之厉”，义无咎也。",
+                "《象》曰：“饮食衎衎”，不素饱也。",
+                "《象》曰：“夫征不复”，离群丑也。“妇孕不育”，失其道也。“利用御寇”，顺相保也。",
+                "《象》曰：“或得其桷”，顺以巽也。",
+                "《象》曰：“终莫之胜吉”，得所愿也。",
+                "《象》曰：“其羽可用为仪，吉”，不可乱也。"
+            };
+        }
+    }
+}
