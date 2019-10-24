d7dd30b5288bb59af105e93c5f5cf7cbc3fbf6a8
LXZ <gdnyfcuso@163.com>
2019-10-24 21:13:56 +0800

    添加丰卦

---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/FuGua/Feng.cs
@@ -0,0 +1,47 @@
+﻿using ImaginaryLearning.Core.BaGua;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core
+{
+  public  class Feng : FuGua
+    {
+        public Feng(PointF startPoint, int totalWidth, int midWidth, int heigth) : base(startPoint, totalWidth, midWidth, heigth)
+        {
+        }
+
+        protected override void InitFuGua(PointF startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            ShangGua = new Zhen(startPoint, totalWidth, midWidth, heigth);
+            XiaGua = new Li(CreateXiaGuaPoint(startPoint, heigth), totalWidth, midWidth, heigth);
+            Name = "丰";
+            PinYin = "Feng";
+            XuGua = 55;
+            GuaCi = "亨，王假之。勿忧，宜日中。";
+            GuaXiangCi = "《象》曰：雷电皆至，丰。君子以折狱致刑。";
+            TuanCi = "《彖》曰：丰，大也。明以动，故丰。“王假之”，尚大也。“勿忧宜日中”，宜照天下也。日中则昃，月盈则食，天地盈虚，与时消息，而况于人乎，况于鬼神乎？";
+            YaoCi = new string[]
+            {
+                "初九，遇其配主，虽旬无咎，往有尚。",
+                "六二，丰其蔀，日中见斗。往得疑疾，有孚发若，吉。",
+                "九三，丰其沛，日中见沫，折其右肱，无咎。",
+                "九四，丰其蔀，日中见斗，遇其夷主，吉。",
+                "六五，来章有庆誉，吉。",
+                "上六，丰其屋，蔀其家，窥其户，阒其无人，三岁不觌，凶。"
+
+            };
+            XiangCi = new string[]
+            {
+                "《象》曰：“虽旬无咎”，过旬灾也。",
+                "《象》曰：“有孚发若”，信以发志也。",
+                "《象》曰：“丰其沛”，不可大事也。“折其右肱”，终不可用也。",
+                "《象》曰：“丰其蔀”，位不当也。“日中见斗”，幽不明也。“遇其夷主”，吉行也。",
+                "《象》曰：六五之吉，有庆也。",
+                "《象》曰：“丰其屋”，天际翔也。“窥其户，阒其无人”，自藏也。"
+            };
+
+        }
+    }
+}
---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/FuGua/LVA.cs
@@ -0,0 +1,48 @@
+﻿using ImaginaryLearning.Core.BaGua;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core
+{
+    /// <summary>
+    /// 火山旅
+    /// </summary>
+    public class LVA : FuGua
+    {
+        public LVA(PointF startPoint, int totalWidth, int midWidth, int heigth) : base(startPoint, totalWidth, midWidth, heigth)
+        {
+        }
+
+        protected override void InitFuGua(PointF startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            ShangGua = new Li(startPoint, totalWidth, midWidth, heigth);
+            XiaGua = new Gen(CreateXiaGuaPoint(startPoint, heigth), totalWidth, midWidth, heigth);
+            Name = "旅";
+            PinYin = "Lv";
+            GuaCi = "小亨。旅贞吉。";
+            TuanCi = "《彖》曰：“旅小亨”，柔得中乎外，而顺乎刚，止而丽乎明，是以“小亨旅贞吉”也。旅之时义大矣哉！";
+            GuaXiangCi = "《象》曰：山上有火，旅。君子以明慎用刑而不留狱。";
+            XuGua = 56;
+            YaoCi = new string[]
+            {
+                "初六，旅琐琐，斯其所取灾。",
+                "六二，旅即次，怀其资，得童仆，贞。",
+                "九三，旅焚其次，丧其童仆，贞厉。",
+                "九四，旅于处，得其资斧，我心不快。",
+                "六五，射雉，一矢亡，终以誉命。",
+                "上九，鸟焚其巢，旅人先笑后号咷。丧牛于易，凶。"
+            };
+            XiangCi = new string[]
+            {
+                "《象》曰：“旅琐琐”，志穷灾也。",
+                "《象》曰：“得童仆贞”，终无尤也。",
+                "《象》曰：“旅焚其次”，亦以伤矣。以旅与下，其义丧也。",
+                "《象》曰：“旅于处”，未得位也。“得其资斧”，心未快也。",
+                "《象》曰：“终以誉命”，上逮也。",
+                "《象》曰：以旅在上，其义焚也。“丧牛于易”，终莫之闻也。"
+            };
+        }
+    }
+}
