53b0578fce8cd5f446a645523e0fb8a3e6a9c5b0
gdnyfcuso <gdnyfcuso@163.com>
2019-10-27 11:35:13 +0800

    添加兑卦

---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/FuGua/DuiA.cs
@@ -0,0 +1,49 @@
+﻿using ImaginaryLearning.Core.BaGua;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core
+{
+    /// <summary>
+    /// 兑
+    /// </summary>
+    public class DuiA : FuGua
+    {
+        public DuiA(PointF startPoint, int totalWidth, int midWidth, int heigth) : base(startPoint, totalWidth, midWidth, heigth)
+        {
+        }
+
+        protected override void InitFuGua(PointF startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            ShangGua = new Dui(startPoint, totalWidth, midWidth, heigth);
+            XiaGua = new Dui(CreateXiaGuaPoint(startPoint, heigth), totalWidth, midWidth, heigth);
+            Name = "兑";
+            PinYin = "Dui";
+            XuGua = 58;
+            IsTwelvePIGua = true;
+            GuaCi = "亨。利贞。";
+            GuaXiangCi = "《象》曰：丽泽，兑。君子以朋友讲习。";
+            TuanCi = "《彖》曰：兑，说也。刚中而柔外，说以利贞，是以顺乎天而应乎人。说以先民，民忘其劳。说以犯难，民忘其死。说之大，民劝矣哉！";
+            YaoCi = new string[]
+            {
+                "初九，和兑，吉。",
+                "九二，孚兑，吉，悔亡。",
+                "六三，来兑，凶。",
+                "九四，商兑未宁，介疾有喜。",
+                "九五，孚于剥，有厉。",
+                "上六，引兑。"
+            };
+            XiangCi = new string[]
+            {
+                "《象》曰：“和兑之吉”，行未疑也。",
+                "《象》曰：“孚兑之吉”，信志也。",
+                "《象》曰：“来兑之凶”，位不当也。",
+                "《象》曰：“九四之喜”，有庆也。",
+                "《象》曰：“孚于剥”，位正当也。",
+                "《象》曰：上六“引兑”，未光也。"
+            };
+        }
+    }
+}
---
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/FuGua/XunA.cs
@@ -0,0 +1,47 @@
+﻿using ImaginaryLearning.Core.BaGua;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImaginaryLearning.Core
+{
+    /// <summary>
+    /// 巽
+    /// </summary>
+    public class XunA : FuGua
+    {
+        public XunA(PointF startPoint, int totalWidth, int midWidth, int heigth) : base(startPoint, totalWidth, midWidth, heigth)
+        {
+        }
+
+        protected override void InitFuGua(PointF startPoint, int totalWidth, int midWidth, int heigth)
+        {
+            ShangGua = new Xun(startPoint, totalWidth, midWidth, heigth);
+            XiaGua = new Xun(CreateXiaGuaPoint(startPoint, heigth), totalWidth, midWidth, heigth);
+            Name = "巽";
+            PinYin = "Xun";
+            XuGua = 57;
+            GuaCi = "小亨。利有攸往。利见大人。";
+            GuaXiangCi = "《象》曰：随风，巽。君子以申命行事。";
+            TuanCi = "《彖》曰：重巽以申命。刚巽乎中正而志行。柔皆顺乎刚，是以“小亨，利有攸往，利见大人”。";
+            YaoCi = new string[]
+            {
+                "初六，进退，利武人之贞。",
+                "九二，巽在床下，用史巫纷若，吉，无咎。",
+                "九三，频巽，吝。",
+                "六四，悔亡，田获三品。",
+                "九五，贞吉，悔亡，无不利，无初有终。先庚三日，后庚三日，吉。"
+                "上九，巽在床下，丧其资斧，贞凶。"
+            };
+            XiangCi = new string[] {
+                "《象》曰：“进退”，志疑也。“利武人之贞”，志治也。",
+                "《象》曰：“纷若之吉”，得中也。",
+                "《象》曰：“频巽之吝”，志穷也。",
+                "《象》曰：“田获三品”，有功也。",
+                "《象》曰：九五之吉，位正中也。",
+                "《象》曰：“巽在床下”，上穷也。“丧其资斧”，正乎凶也。"
+            };
+        }
+    }
+}
