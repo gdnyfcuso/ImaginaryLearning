d31edbaa182c3ea194bb6bef0d1d384841a8949e
LXZ <gdnyfcuso@163.com>
2019-11-19 21:52:22 +0800

    添加复卦需要实现的方法

--- ImaginaryLearning.Core/ImaginaryLearning.Core/Base/LiuSiGuaCoordinateSystem.cs
+++ ImaginaryLearning.Core/ImaginaryLearning.Core/Base/LiuSiGuaCoordinateSystem.cs
@@ -6,17 +6,74 @@ using System.Text;
 namespace ImaginaryLearning.Core.Base
 {
     /// <summary>
-    /// 六十四卦所用的从标系统
+    /// 六十四卦所用的坐标系统
     /// </summary>
     public class LiuSiGuaCoordinateSystem
     {
         /// <summary>
-        /// 
+        ///创建六十四卦圆图
         /// </summary>
         /// <returns></returns>
-        public Bitmap CreateLiuSiGuaBitmap()
+        public Bitmap CreateLiuSiGuaBitmap(Bitmap sourceBitmap)
         {
             return null;
         }
+
+        /// <summary>
+        /// 创建六十四卦方图
+        /// </summary>
+        /// <returns></returns>
+        public Bitmap CreateLiuSiGuaRectangleBitmap(Bitmap sourceBitmap)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 通过数字计算复卦
+        /// </summary>
+        /// <returns></returns>
+        public FuGua CreateFuGuaByNumber()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 通过时间计算复卦
+        /// </summary>
+        /// <param name="SourceDateTime"></param>
+        /// <returns></returns>
+        public FuGua CreateFuGuaByDateTime(DateTime SourceDateTime)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 通过当前时间计算复卦
+        /// </summary>
+        /// <returns></returns>
+        public FuGua CreateFuGuaByCurrentDateTime()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 通过手机号计算复卦
+        /// </summary>
+        /// <returns></returns>
+        public FuGua CreateFuGuaByPhoneNumber()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// 将复卦画到图像上去
+        /// </summary>
+        /// <param name="sourceBitmap"></param>
+        /// <returns></returns>
+        public Bitmap DrawingFuGuaToBitmap(Bitmap sourceBitmap)
+        {
+            return null;
+        }
+
     }
 }
